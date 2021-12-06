6ba91b67c6dc1ab9f4b480f1ccf726f82ed56f86
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-06 14:04:29 +0100

    make bired reappaer on left size

--- main.js
+++ main.js
@@ -4,16 +4,21 @@ window,addEventListener('load', main);
 let imageElement;
 
 const speed = .4;
+const size = 40;
 const position = {
     let: 0,
     left: 0
 }
 function main() {
     imageElement = document.querySelector('img')
+    imageElement.style.width = size + '%';
     setInterval(moveBird, 20)
 }
 
 function moveBird() {
-    position.left += speed;
-    imageElement.style.left = position.left + '%'
+    position.left +=  speed;
+    if (position.left > 100) {
+        position.left = -40;
+    }
+    imageElement.style.left = position.left + '%';
 }
