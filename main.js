8a34aec4df187f23546c6975b44827b8bc339f90
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-06 14:31:48 +0100

    make the bired moving around the bage

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 window,addEventListener('load', main);
 
-/** @type {HTMLElement} */
+/** @type {HTMLImageElement} */
 let imageElement;
 
-const speed ={
-    x:  .4,
-    y: .2,
+//state 
+const size = 40;
+const speed = {
+    x:  0.4,
+    y: 0.2,
+}
+const position = {
+    top: 0,
+    left: -size,
 }
 
-const size = 40;
-const position = {
-    let: 0,
-    top: 0
-}
 function main() {
-    imageElement = document.querySelector('img')
+    imageElement = document.querySelector('img');
     imageElement.style.width = size + '%';
-    setInterval(moveBird, 20)
+    setInterval(moveBird, 20);
+    setInterval(updateSpeed, 1000);
+  }
+  
+function updateSpeed() {
+    speed.x = -0.5 + Math.random();
+    speed.y = -0.5 + Math.random();
+    if (speed.x < 0) {
+        imageElement.style.transform = 'scaleX(-1)';
+    } else {
+        imageElement.style.transform = 'scaleX(1)';
+    }
 }
 
 function moveBird() {
-    position.top +=  speed.x;
-    if (position.top > 100) {
-        position.top = -size;
-    }
-    imageElement.style.left = position.top + '%';
+    position.left +=  speed.x;
+    if (position.left > 100) {
+        position.left = -size;
+    } else if (position.left < -size) {
+        position.left = 100;   
+     }
+    imageElement.style.left = position.left + '%';
 
 
     position.top +=  speed.y;
     if (position.top > 100) {
         position.top = -size;
-    }
+    }else if (position.top < -size) {
+        position.top = 100;   
+     }
     imageElement.style.top = position.top + '%';
 }
